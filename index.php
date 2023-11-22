fb3abc8d0b9417c371307bd99c1614013ce6e311
Shahram <ardalan.shahram@gmail.com>
2023-11-22 13:15:55 +0330

    change index file

--- index.php
+++ index.php
@@ -19,30 +19,40 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="productName">
                         Product Name
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="productName" id="productName" type="text" placeholder="Product Name...">
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="productName" id="productName" type="text" placeholder="Product Name...">
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
                         Description
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="description" id="description" type="text" placeholder="Description...">
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="description" id="description" type="text" placeholder="Description...">
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="quantity">
                         quantity
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="quantity" id="quantity" type="text" placeholder="Product Name...">
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="quantity" id="quantity" type="text" placeholder="Product Name...">
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="color">
                         Color
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="color" id="color" type="text" placeholder="Product Name...">
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="color" id="color" type="text" placeholder="Product Name...">
                 </div>
                 <div class="md:flex md:items-center">
                     <div class="md:w-1/3"></div>
                     <div class="md:w-2/3">
-                        <button class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                        <button
+                            class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                            type="submit">
                             Add to list
                         </button>
                     </div>
@@ -61,7 +71,6 @@
                 echo renderProducts($_POST["productName"], $_POST["description"], $_POST["quantity"], $_POST["color"]);
             }
             ?>
-
         </div>
     </div>
 </body>
